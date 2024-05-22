33c5b1c23b5f44b088e0ed8a0bbb1916a93e0d75
Joel <jjalixtoc@gmail.com>
2024-05-22 17:13:24 -0500

    terminado

--- src/main/java/com/joel/screenmath/principal/Principal.java
+++ src/main/java/com/joel/screenmath/principal/Principal.java
@@ -1,10 +1,18 @@
 package com.joel.screenmath.principal;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.joel.screenmath.model.DatosEpisodio;
 import com.joel.screenmath.model.DatosSerie;
@@ -62,7 +70,11 @@ public class Principal {
         System.out.println("Top 5 episodios");
         datosEpisodios.stream()
                     .filter(e -> !e.evaluacion().equalsIgnoreCase("N/A"))
+                    .peek(e -> System.out.println("Primer filtro (N/A)" + e))
                     .sorted(Comparator.comparing(DatosEpisodio::evaluacion).reversed())
+                    .peek(e -> System.out.println("Segundo ordenacion (M>m)" + e))
+                    .map(e -> e.titulo().toUpperCase())
+                    .peek(e -> System.out.println("Tercer filtro Mayusula (m>M)" + e))
                     .limit(5)
                     .forEach(System.out::println);
 
@@ -71,6 +83,52 @@ public class Principal {
                             .flatMap(t -> t.episodios().stream()
                             .map(d -> new Episodio(t.numero(),d)))
                             .collect(Collectors.toList());
-        episodios.forEach(System.out::println);
+        // episodios.forEach(System.out::println);
+        
+        //busqueda de episodios a partir de x año
+        // System.out.println("Por favor indica el año desde donde quieres ver");
+        // var fecha = teclado.nextInt();
+        // teclado.nextLine();
+
+        // LocalDate fechaBusqueda = LocalDate.of(fecha, 1, 1);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        // episodios.stream()
+        //         .filter(e -> e.getFechaDeLanzamiento() != null && e.getFechaDeLanzamiento().isAfter(fechaBusqueda))
+        //         .forEach(e -> System.out.println(
+        //             "Temporada "+ e.getTemporada() + 
+        //             "Episodio "+ e.getTitulo() + 
+        //             "Fecha de Lanzamiento " + e.getFechaDeLanzamiento().format(dtf)
+        //         ));
+
+        //busca episodios por pedazo del titulo
+        // System.out.println("Por favor escriba el titulo del episodio que desea ver");
+        // var pedazoTitulo = teclado.nextLine();
+        // Optional<Episodio> episodioBuscado = episodios.stream()
+        //         .filter(e -> e.getTitulo().toUpperCase().contains(pedazoTitulo.toUpperCase()))
+        //         .findFirst();
+        // if(episodioBuscado.isPresent()){
+        //     System.out.println("Episodio Encontrado");
+        //     System.out.println("Los datos son: "+ episodioBuscado.get().getTitulo());
+        // }else{
+        //     System.out.println("Episodio no encontrado");
+        // }
+
+        Map<Integer , Double> evaluacionPorTemporada = episodios.stream()
+                        .filter(e -> e.getEvaluacion() > 0.0)
+                        .collect(Collectors.groupingBy(Episodio::getTemporada,
+                                Collectors.averagingDouble(Episodio::getEvaluacion)));
+        System.out.println(evaluacionPorTemporada);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                        .filter(e -> e.getEvaluacion() > 0.0)
+                        .collect(Collectors.summarizingDouble(Episodio::getEvaluacion));
+        System.out.println(est);
+        System.out.println("Media de las evaluaciones: "+ est.getAverage());
+        System.out.println("Media de las evaluaciones: "+ est.getCount());
+        System.out.println("Media de las evaluaciones: "+ est.getMax());
+        System.out.println("Media de las evaluaciones: "+ est.getMin());
+        System.out.println("Media de las evaluaciones: "+ est.getSum());
+
     }
 }
